36e801b5c3ae3f4948d8c074b7c44722d025d782
Benqing Shen <benqingshen@gmail.com>
2015-07-18 16:37:04 -0500

    dummy everything. use log1p optimization

--- R_Scripts/train_tube.R
+++ R_Scripts/train_tube.R
@@ -9,20 +9,24 @@ set.seed(0)
 
 
 train_set <- tbl_df(read.csv('./competition_data/train_set.csv'))
-train_set_supplier_dummies <- tbl_df(read.csv('./train_set_supplier_dummies.csv'))
-tube <- tbl_df(read.csv('./competition_data/tube.csv'))
+train_set <- tbl_df(read.csv('./train_set_supplier_dummies.csv'))
+train_set <- tbl_df(read.csv('./train_set_no_date_dummies_drop_bracket_n.csv'))
+# tube <- tbl_df(read.csv('./competition_data/tube.csv'))
+# tube <- tbl_df(read.csv('./tube_material_id_imputed.csv'))
+tube <- tbl_df(read.csv('./tube_material_id_imputed_dummies_drop_ns.csv'))
 
 spec_dummies <- tbl_df(read.csv('./spec_dummies.csv'))
 comp_type_dummies <- tbl_df(read.csv('./comp_type_dummies.csv'))
 
 # train_tube <- dplyr::left_join(train_set, tube, by = 'tube_assembly_id')
-train_tube <- tbl_df(merge(train_set_supplier_dummies, tube))
+train_tube <- tbl_df(merge(train_set, tube))
 train_tube <- tbl_df(merge(train_tube, comp_type_dummies))
+train_tube <- tbl_df(merge(train_tube, spec_dummies))
 
-material_na_mask <- is.na(train_tube$material_id)
-train_tube$material_id <- as.character(train_tube$material_id)
-train_tube$material_id[material_na_mask] <- '0'
-train_tube$material_id <- as.factor(train_tube$material_id)
+# material_na_mask <- is.na(train_tube$material_id)
+# train_tube$material_id <- as.character(train_tube$material_id)
+# train_tube$material_id[material_na_mask] <- '0'
+# train_tube$material_id <- as.factor(train_tube$material_id)
 
 trainIndex <- createDataPartition(train_tube$cost, p = 0.9, list = FALSE)
 
@@ -35,13 +39,13 @@ fit <- rpart(cost ~ supplier + annual_usage + quantity +
 # in_sample <- predict(fit, newdata = train_tube)
 # rmsle(train_tube$cost, in_sample)
 
-rf.fit <- randomForest(cost ~ . - tube_assembly_id - quote_date,
+rf.fit <- randomForest(log1p(cost) ~ . - tube_assembly_id - quote_date,
                   data = train_tube, subset = trainIndex,
                   ntree = 200, do.trace = 5)
 in_sample <- predict(rf.fit, newdata = train_tube[trainIndex, ])
 out_sample <- predict(rf.fit, newdata = train_tube[-trainIndex, ])
-rmsle(train_tube[trainIndex, 'cost'], in_sample)
-rmsle(train_tube[-trainIndex, 'cost'], out_sample)
+rmsle(train_tube[trainIndex, 'cost'], expm1(in_sample))
+rmsle(train_tube[-trainIndex, 'cost'], expm1(out_sample))
 
 train_tube_features <- train_tube %>% select(annual_usage, quantity, material_id,
                         diameter, wall, length, num_bends, bend_radius,
